c51331588b4e36d7e29dbae05b92cdd05c93f9c5
Oyekunle Oloyede <oyekunlemac@gmail.com>
2019-11-01 09:37:25 +0100

    Improved the comment

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -18,20 +18,21 @@ def reconstruct_trip(tickets, length):
 
     # loop through the tickets
     for ticket in tickets:
-        # add the tickets source/destination to the hashtable as pair
+        # add the tickets source/destination to the hashtable as a pair
         hash_table_insert(hashtable, ticket.source, ticket.destination)
 
     # set a variable source to string None
     source = "NONE"
     # set a variable position to 0
     position = 0
+
     # loop while position is less than length
     while position < length:
-        # get the destination for the source
+        # get the destination for the source from the hashtable
         destination = hash_table_retrieve(hashtable, source)
         # set the value of source in route using the position
         route[position] = destination
-        # set source to the destination
+        # set source to the destination for the next iteration
         source = destination
         # increment position
         position += 1
